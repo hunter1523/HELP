4c3691f6bbc666d8398ea453161803326b24ad1a
hunter1523 <92686402+hunter1523@users.noreply.github.com>
2022-11-18 14:11:16 -0300

    Create ejercisio14_02.c

---
+++ ejercisio14_02.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define VALOR ((int)10)
+
+int main (void)
+{
+  int i1=0, i2=0, m, n;
+  int v[VALOR];
+
+  printf(" Adivine el numero misterioso. \r \n \n \n");
+
+  srand(time(NULL));
+  m = rand() % 100;
+
+  while ((m!=v[i1]) && (i1<=VALOR)) {
+        printf(" Ingrese un numero: \r \n");
+        scanf ("%d", &v[i1]);
+        while ((i2 < VALOR) && (v[i2] != v[i1])) {
+          i2 += 1;
+        }
+        if (v[i2] != v[i1]) {
+          i1 += 1;
+        }else{
+          printf(" Numero repetido. \n \n");
+        }
+      }
+  if (m==v[i1]) {
+    printf(" Felicidades acertaste el numero misterioso\r \n");
+  } else {
+    printf(" Fallaste, el numero misterioso era: \n %d \r \n", m);
+  }
+return (0);
+}
